0ffd90e50f5ee8085184b6b8a7b03b739323731a
strabon <strabon@Mac-mini.local>
2017-07-09 17:50:42 +0200

    up
    up

--- Sources/App/main.swift
+++ Sources/App/main.swift
@@ -11,25 +11,27 @@ do {
     assertionFailure("Error adding provider: \(error)")
 }
 
+// user pages routings
+
+// home page
+// http://0.0.0.0:8080
 drop.get { req in
     return try drop.view.make("welcome", [
     	"message": drop.localization[req.lang, "welcome", "title"]
     ])
 }
 
-drop.get("all") { req in
-    let friends = try Friend.all().makeNode()
-    let friendsDictionary = ["friends": friends]
-    return try JSON(node: friendsDictionary)
+// simple test for server  ( it is a REST JSON, to be moved .. )
+// http://0.0.0.0:8080/data/1
+drop.get("data", Int.self) { request, int in
+    return try JSON(node: [
+        "int": int,
+        "name": request.data["name"]?.string ?? "no name"
+    ])
 }
 
-drop.get("friends", Int.self) { req, userID in
-    guard let friend = try Friend.find(userID) else {
-        throw Abort.notFound
-    }
-    return try friend.makeJSON()
-}
-
+// some test  ( hello page )
+// http://0.0.0.0:8080/hello
 drop.get("hello") { request in
   let name = request.data["name"]?.string ?? "stranger"
 
@@ -38,18 +40,100 @@ drop.get("hello") { request in
   ])
 }
 
-
-drop.post("friend") { req in
-    var friend = try Friend(node: req.json)
-    try friend.save()
-    return try friend.makeJSON()
-}
-
+// some test  ( page with image )
 drop.get("html") { req in
   let response = Response(status: .ok, body: "<html><img src=http://www.w3schools.com/html/pic_mountain.jpg></html>")
   response.headers["Content-Type"] = "text/html"
   return response
 }
+
+//===========================
+//  REST JSON
+//===========================
+
+//===========================
+// GET all
+// http://0.0.0.0:8080/all
+drop.get("all") { req in
+    let friends = try Friend.all().makeNode()
+    let friendsDictionary = ["friends": friends]
+    return try JSON(node: friendsDictionary)
+}
+
+//===========================
+// GET single record
+// http://0.0.0.0:8080/friend/1     GET method
+drop.get("friend", Int.self) { req, userID in
+
+  // what is UserID
+  drop.log.info("get  req:   userID=\(userID)")
+    guard let friend = try Friend.find(userID) else {
+        throw Abort.notFound
+    }
+    return try friend.makeJSON()
+}
+
+//===========================
+// POST a new record
+/*
+  curl -X POST -H "Content-Type: application/json" -d '{ "age": 60, "email": "OSZ99@e.com", "id": 3333, "name": "Yet Other Other brother"  }' http://0.0.0.0:8080/friend
+*/
+drop.post("friend") { req in
+    //drop.log.info("post  req:\(req.json)")
+    var friend = try Friend(node: req.json)
+    try friend.save()
+    return try friend.makeJSON()
+}
+
+//::::: the same !!!! different page name ...  with trace
+drop.post("user") { req in
+    let json = req.json
+
+    //
+    drop.log.info("post  req:\(json)")
+
+    var friend = try Friend(node: req.json)
+    try friend.save()
+    return try friend.makeJSON()
+}
+//:::::
+
+//===========================
+// DELETE a new record
+// http://0.0.0.0:8080/friend/1     DELETE method
+drop.delete("friend", Int.self) { req, userID in
+
+  // what is UserID
+  drop.log.info("delete  req:   userID=\(userID)")
+  // should test and delete here....
+
+  // what to return ? here indicate userID that has been deleted ?
+  return try JSON(node: [
+      "userID": userID
+    ])
+  }
+
+//===========================
+// PUT in order to change a record
+// shouldl test a PUT for changing a record ?
+// http://0.0.0.0:8080/friends/1     PUT method
+drop.put("friend", Int.self) { req, userID in
+
+  // what is UserID
+  drop.log.info("put  req:   userID=\(userID)")
+  // should test and delete here....
+
+  // what to return ? here indicate userID that has been deleted ?
+  return try JSON(node: [
+      "userID": userID
+    ])
+  }
+
+// ------ end of REST JSON
+//===========================
+
+
+//  Vapor incantations....
 
 drop.resource("posts", PostController())
 
---
+++ exercise.sh
@@ -0,0 +1,7 @@
+#
+curl -X POST -H "Content-Type: application/json" -d '{ "age": 60, "email": "OSZ990@e.com", "id": 2001, "name": "Mr 2001"  }' http://0.0.0.0:8080/user
+curl -X POST -H "Content-Type: application/json" -d '{ "age": 61, "email": "OSZ991@e.com", "id": 2002, "name": "Mr 2002"  }' http://0.0.0.0:8080/user
+curl -X POST -H "Content-Type: application/json" -d '{ "age": 62, "email": "OSZ992@e.com", "id": 2003, "name": "Mr 2003"  }' http://0.0.0.0:8080/user
+curl -X POST -H "Content-Type: application/json" -d '{ "age": 63, "email": "OSZ993@e.com", "id": 2004, "name": "Mr 2004"  }' http://0.0.0.0:8080/user
+
+
